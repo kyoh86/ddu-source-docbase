70b58da023ec04950f8ec95f5b058d174b183d14
GitHub <noreply@github.com>
2023-09-22 02:45:24 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/docbase_posts.ts
+++ denops/@ddu-sources/docbase_posts.ts
@@ -1,5 +1,5 @@
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
-import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.0/file.ts";
+import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import type { Item } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import { BaseSource } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import {
