7810f9306d5a746c3434361f435c000137d4f44a
GitHub <noreply@github.com>
2024-08-03 09:04:11 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/docbase_templates.ts
+++ denops/@ddu-sources/docbase_templates.ts
@@ -3,7 +3,7 @@ import type { ActionData as FileActionData } from "jsr:@shougo/ddu-kind-file@~0.
 import type { Item } from "jsr:@shougo/ddu-vim@~5.0.0/types";
 import { BaseSource } from "jsr:@shougo/ddu-vim@~5.0.0/types";
 import { isPost, type Post } from "jsr:@kyoh86/denops-docbase-vim@~0.1.0/types";
-import { ensure, is } from "jsr:@core/unknownutil@~3.18.1";
+import { ensure, is } from "jsr:@core/unknownutil@~4.0.0";
 
 type ActionData = FileActionData & Post;
 
